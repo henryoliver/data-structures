d106079f0ffa86bb8cc6c70972c7a723baf5e20e
Henry Oliver <henry_oliver@me.com>
2020-07-02 16:00:29 -0400

    feat: add the linked list data structure

---
+++ linkedList.js
@@ -0,0 +1,426 @@
+// Classical inheritance approach
+class ClassLinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    append(value = '') {
+        const node = { data: value, next: null };
+
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            let currNode = this.head;
+
+            // Traverse linked list till its tail
+            while (currNode.next !== null) {
+                currNode = currNode.next;
+            }
+
+            currNode.next = node;
+        }
+
+        this.size++;
+    }
+
+    prepend(value = '') {
+        const node = { data: value, next: this.head };
+
+        this.head = node;
+        this.size++;
+    }
+
+    insertAt(value = '', index = 0) {
+        if (index >= 0 && index <= this.size) {
+            const node = { data: value, next: null };
+
+            if (index === 0) {
+                node.next = this.head;
+
+                this.head = node;
+            } else {
+                let prevNode;
+                let currIndex = 0;
+                let currNode = this.head;
+
+                while (currIndex < index) {
+                    currIndex++;
+                    prevNode = currNode;
+                    currNode = currNode.next;
+                }
+
+                node.next = currNode;
+                prevNode.next = node;
+            }
+
+            this.size++;
+        }
+    }
+
+    removeAt(index = 0) {
+        if (index >= 0 && index <= this.size) {
+            let currNode = this.head;
+
+            if (index === 0) {
+                this.head = currNode.next;
+            } else {
+                let prevNode;
+                let currIndex = 0;
+
+                while (currIndex < index) {
+                    currIndex++;
+                    prevNode = currNode;
+                    currNode = currNode.next;
+                }
+
+                prevNode.next = currNode.next;
+            }
+
+            this.size--;
+
+            return currNode.data;
+        }
+    }
+
+    getAt(index = 0) {
+        if (index >= 0 && index <= this.size) {
+            if (index === 0) {
+                return this.head;
+            } else {
+                let currIndex = 0;
+                let currNode = this.head;
+
+                while (currIndex < index) {
+                    currNode = currNode.next;
+                    currIndex++;
+                }
+
+                return currNode;
+            }
+        }
+    }
+
+    getSize() {
+        return this.size;
+    }
+}
+
+const newClassLinkedList = new ClassLinkedList();
+
+
+// Prototypal inheritance approach
+function PrototypalLinkedList() {
+    this.head = null;
+    this.size = 0;
+}
+
+PrototypalLinkedList.prototype.append = function(value = '') {
+    const node = { data: value, next: null };
+
+    if (this.head === null) {
+        this.head = node;
+    } else {
+        let currNode = this.head;
+
+        // Traverse linked list till its tail
+        while (currNode.next !== null) {
+            currNode = currNode.next;
+        }
+
+        currNode.next = node;
+    }
+
+    this.size++;
+}
+
+PrototypalLinkedList.prototype.prepend = function(value = '') {
+    const node = { data: value, next: this.head };
+
+    this.head = node;
+    this.size++;
+}
+
+PrototypalLinkedList.prototype.insertAt = function(value = '', index = 0) {
+    if (index >= 0 && index <= this.size) {
+        const node = { data: value, next: null };
+
+        if (index === 0) {
+            node.next = this.head;
+
+            this.head = node;
+        } else {
+            let prevNode;
+            let currIndex = 0;
+            let currNode = this.head;
+
+            while (currIndex < index) {
+                currIndex++;
+                prevNode = currNode;
+                currNode = currNode.next;
+            }
+
+            node.next = currNode;
+            prevNode.next = node;
+        }
+
+        this.size++;
+    }
+}
+
+PrototypalLinkedList.prototype.removeAt = function(index = 0) {
+    if (index >= 0 && index <= this.size) {
+        let currNode = this.head;
+
+        if (index === 0) {
+            this.head = currNode.next;
+        } else {
+            let prevNode;
+            let currIndex = 0;
+
+            while (currIndex < index) {
+                currIndex++;
+                prevNode = currNode;
+                currNode = currNode.next;
+            }
+
+            prevNode.next = currNode.next;
+        }
+
+        this.size--;
+
+        return currNode.data;
+    }
+}
+
+PrototypalLinkedList.prototype.getAt = function(index = 0) {
+    if (index >= 0 && index <= this.size) {
+        if (index === 0) {
+            return this.head;
+        } else {
+            let currIndex = 0;
+            let currNode = this.head;
+
+            while (currIndex < index) {
+                currNode = currNode.next;
+                currIndex++;
+            }
+
+            return currNode;
+        }
+    }
+}
+
+PrototypalLinkedList.prototype.getSize = function() {
+    return this.size;
+}
+
+const newPrototypalLinkedList = new PrototypalLinkedList();
+
+
+// Prototypal inheritance Object.create approach
+const objectCreateLinkedList = {
+    init: {
+        head: { value: null, writable: true }, 
+        size: { value: 0, writable: true }
+    },
+    append: function(value = '') {
+        const node = { data: value, next: null };
+
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            let currNode = this.head;
+
+            // Traverse linked list till its tail
+            while (currNode.next !== null) {
+                currNode = currNode.next;
+            }
+
+            currNode.next = node;
+        }
+
+        this.size++;
+    },
+    prepend: function(value = '') {
+        const node = { data: value, next: this.head };
+
+        this.head = node;
+        this.size++;
+    },
+    insertAt: function(value = '', index = 0) {
+        if (index >= 0 && index <= this.size) {
+            const node = { data: value, next: null };
+
+            if (index === 0) {
+                node.next = this.head;
+
+                this.head = node;
+            } else {
+                let prevNode;
+                let currIndex = 0;
+                let currNode = this.head;
+
+                while (currIndex < index) {
+                    currIndex++;
+                    prevNode = currNode;
+                    currNode = currNode.next;
+                }
+
+                node.next = currNode;
+                prevNode.next = node;
+            }
+
+            this.size++;
+        }
+    },
+    removeAt: function(index = 0) {
+        if (index >= 0 && index <= this.size) {
+            let currNode = this.head;
+
+            if (index === 0) {
+                this.head = currNode.next;
+            } else {
+                let prevNode;
+                let currIndex = 0;
+
+                while (currIndex < index) {
+                    currIndex++;
+                    prevNode = currNode;
+                    currNode = currNode.next;
+                }
+
+                prevNode.next = currNode.next;
+            }
+
+            this.size--;
+
+            return currNode.data;
+        }
+    },
+    getAt: function(index = 0) {
+        if (index >= 0 && index <= this.size) {
+            if (index === 0) {
+                return this.head;
+            } else {
+                let currIndex = 0;
+                let currNode = this.head;
+
+                while (currIndex < index) {
+                    currNode = currNode.next;
+                    currIndex++;
+                }
+
+                return currNode;
+            }
+        }
+    },
+    getSize: function() {
+        return console.log(`The size of this linked list is: ${this.size}`);
+    },
+    printOut: function() {
+        return console.log(JSON.stringify(this.head));
+    }
+};
+
+const newObjectCreateLinkedList = Object.create(objectCreateLinkedList, objectCreateLinkedList.init);
+
+
+// Factory functions approach (composition)
+const factoryLinkedList = ({ head = null, size = 0 } = {}) => ({
+    append: (value = '') => {
+        const node = { data: value, next: null };
+
+        if (head === null) {
+            head = node;
+        } else {
+            let currNode = head;
+
+            // Traverse linked list till its tail
+            while (currNode.next !== null) {
+                currNode = currNode.next;
+            }
+
+            currNode.next = node;
+        }
+
+        size++;
+    },
+    prepend: (value = '') => {
+        const node = { data: value, next: head };
+
+        head = node;
+        size++;
+    },
+    insertAt: (value = '', index = 0) => {
+        if (index >= 0 && index <= size) {
+            const node = { data: value, next: null };
+
+            if (index === 0) {
+                node.next = head;
+
+                head = node;
+            } else {
+                let prevNode;
+                let currIndex = 0;
+                let currNode = head;
+
+                while (currIndex < index) {
+                    currIndex++;
+                    prevNode = currNode;
+                    currNode = currNode.next;
+                }
+
+                node.next = currNode;
+                prevNode.next = node;
+            }
+
+            size++;
+        }
+    },
+    removeAt: (index = 0) => {
+        if (index >= 0 && index <= size) {
+            let currNode = head;
+
+            if (index === 0) {
+                head = currNode.next;
+            } else {
+                let prevNode;
+                let currIndex = 0;
+
+                while (currIndex < index) {
+                    currIndex++;
+                    prevNode = currNode;
+                    currNode = currNode.next;
+                }
+
+                prevNode.next = currNode.next;
+            }
+
+            size--;
+
+            return currNode.data;
+        }
+    },
+    getAt: (index = 0) => {
+        if (index >= 0 && index <= size) {
+            if (index === 0) {
+                return head;
+            } else {
+                let currIndex = 0;
+                let currNode = head;
+
+                while (currIndex < index) {
+                    currNode = currNode.next;
+                    currIndex++;
+                }
+
+                return currNode;
+            }
+        }
+    },
+    getSize: () => console.log(`The size of this linked list is: ${size}`),
+    printOut: () => console.log(JSON.stringify(head))
+});
+
+const newFactoryLinkedList = factoryLinkedList();
---
+++ linkedList.py
@@ -0,0 +1,100 @@
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def append(self, value=''):
+        node = { 'data': value, 'next': None }
+
+        if (self.head == None):
+            self.head = node
+        else:
+            currNode = self.head
+
+            # Traverse linked list till its tail
+            while (currNode.get('next') != None):
+                currNode = currNode.next
+
+            currNode['next'] = node
+
+        self.size += 1
+
+    def prepend(self, value=''):
+        node = { 'data': value, 'next': self.head }
+
+        self.head = node
+        self.size += 1
+
+    def insertAt(self, value='', index=0):
+        if (index >= 0 and index <= self.size):
+            node = { 'data': value, 'next': None }
+
+            if (index == 0):
+                node.next = self.head
+
+                self.head = node
+            else:
+                currIndex = 0
+                currNode = self.head
+
+                while (currIndex < index):
+                    currIndex += 1
+                    prevNode = currNode
+                    currNode = currNode.next
+
+                node.next = currNode
+                prevNode.next = node
+
+            self.size += 1
+
+    def removeAt(self, index=0):
+        if (index >= 0 and index < self.size):
+            currNode = self.head
+
+            if (index == 0):
+                self.head = currNode.next
+            else:
+                currIndex = 0
+
+                while (currIndex < index):
+                    currIndex += 1
+                    prevNode = currNode
+                    currNode = currNode['next']
+
+                prevNode['next'] = currNode['next']
+
+            self.size -= 1
+
+            return print(f'Node removed: {currNode}')
+
+    def getAt(self, index=0):
+        if (index >= 0 and index < self.size):
+            if (index == 0):
+                return print(f'Node: {self.head}')
+            else:
+                currIndex = 0
+                currNode = self.head
+
+                while (currIndex < index):
+                    currNode = currNode['next']
+                    currIndex += 1
+
+                return print(f'Node at index {index} is: {currNode}')
+
+    def getSize(self):
+        return print(f'Linked list size: {self.size}')
+
+    def printOut(self):
+        return print(f'Linked list: {self.head}')
+
+NewLinkedList = LinkedList()
+
+NewLinkedList.append('Node 1')
+NewLinkedList.append('Node 2')
+NewLinkedList.prepend('Node 0')
+NewLinkedList.getAt(2)
+NewLinkedList.getSize()
+NewLinkedList.printOut()
+NewLinkedList.removeAt(2)
+NewLinkedList.printOut()
+NewLinkedList.getSize()
